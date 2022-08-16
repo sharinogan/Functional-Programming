7af8c31ffc70ccd52c6c8604a11456faf8c0aa38
Voragun Supawong <voragun2543@gmail.com>
2022-08-16 19:23:30 +0700

    Example tree

--- code/Start.java
+++ code/Start.java
@@ -1,12 +1,17 @@
 class Start {
 
     public static void main(String[] data) {
-        show();
+        f("");
     }
     
-    static void show() {
-        System.out.println("This is engine");
+    static void f(String status) {
+        if (status.length() == 3) {
+            System.out.println(status);
+            return; // พอถึงขั้นสุดท้าย return กลับขึ้นไป
+        }
+        
+        f(status + "L");
+        f(status + "D"); // optional เผื่อมีการเสมอ
+        f(status + "W");
     }
-}
-
-  
+}    
