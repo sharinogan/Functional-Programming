c26ee2de717b5a6d987b65a0a4806ea77658c080
Voragun Supawong <voragun2543@gmail.com>
2022-08-16 18:59:23 +0700

    first commit

---
+++ code/Start.java
@@ -0,0 +1,12 @@
+class Start {
+    public static void main(String[] data) {
+        Engine e = new Engine();
+        e.show();
+    }
+}
+
+class Engine {
+    void show() {
+        System.out.println("This is engine.");
+    }
+}
