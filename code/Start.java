4f8e9c8730f8f3de085489c95f904ee2fa3bc99b
Voragun Supawong <voragun2543@gmail.com>
2022-08-16 21:33:48 +0700

    Get sum of node in divide and conquer

--- code/Start.java
+++ code/Start.java
@@ -1,19 +1,16 @@
+
 class Start {
+
     public static void main(String[] data) {
-        
-        int[] a = { 8, 5, 4, 3, 6, 3, 4 };
+
+        int[] a = {8, 5, 4, 3, 6, 3, 4};
         Item root = create(a, 0, a.length - 1);
-        int c = count(root);
+        int c = total(root);
         System.out.println(c);
-    }  
-    
+    }
+
     static Item create(int[] a, int left, int right) {
-        if(left > right) return null;
-        if(left == right) {
-            Item x = new Item();
-            x.number = a[left];
-            return x;
-        } 
+        if (left > right) return null;               
         int mid = (left + right) / 2;
         Item x = new Item();
         x.number = a[mid];
@@ -22,13 +19,21 @@ class Start {
         return x;
     }
     
+    static int total(Item e) {
+        return e == null ? 0 : 
+                total(e.left) + total(e.right) + e.number;
+    }
+
     static int count(Item e) {
-        if (e == null) return 0;
+        if (e == null) {
+            return 0;
+        }
         return count(e.left) + count(e.right) + 1;
     }
 }
 
 class Item {
+
     String name;      // null
     int number;       // 0
     double salary;    // 0.0
